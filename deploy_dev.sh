025164ce2adfad36ec95fa552f64f6359420abfe
menza01 <se56160422@gmail.com>
2018-10-09 15:19:56 +0700

    init pro

--- deploy_dev.sh
+++ deploy_dev.sh
@@ -6,4 +6,4 @@ curl \
   -F "notes_type=0" \
   -F "ipa=@app/build/outputs/apk/debug/app-debug.apk" \
   -H "X-HockeyAppToken : 4bac0a6e9f054038bbb899acf442d08a" \
-  https://rink.hockeyapp.net/api/2/apps/aadf48671f444a30ac25a22f7aabf828/app_versions/upload
+  https://rink.hockeyapp.net/api/2/apps/5cc3aab44c444673814c53933a84a62f/app_versions/upload
