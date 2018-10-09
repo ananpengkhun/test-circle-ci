993c9d6cd9972c09eeabf3748665757225eb1380
menza01 <se56160422@gmail.com>
2018-10-09 11:41:08 +0700

    init pro

---
+++ .circleci/deploy_dev.sh
@@ -0,0 +1,7 @@
+
+curl \
+-F "status=2" \
+-F "notify=0" \
+-F "ipa=@AppName/build/outputs/apk/app-debug.apk" \
+-H "X-HockeyAppToken: 4bac0a6e9f054038bbb899acf442d08a" \
+https://rink.hockeyapp.net/api/2/apps/com.example.ananpengkhun.mycircleci/app_versions/upload
