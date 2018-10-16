d0e863f1ca7fd1e47135e0f48fefab213027b638
menza01 <se56160422@gmail.com>
2018-10-16 11:56:53 +0700

    init pro

--- increase_version_code.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-MANIFEST_FILE='AndroidManifest.xml'
-VERSIONCODE=`grep versionCode $MANIFEST_FILE | sed 's/.*versionCode="//;s/".*//'`
-VERSIONNAME=`grep versionName $MANIFEST_FILE | sed 's/.*versionName="//;s/\.[0-9]*".*//'`
-NEWCODE=$BUILD_NUMBER
-NEWNAME=$VERSIONNAME.$BUILD_NUMBER
-echo "Updating Android build information. New version code: $NEWCODE - New version name: $NEWNAME";
-sed -i 's/versionCode *= *"'$VERSIONCODE'"/versionCode="'$NEWCODE'"/; s/versionName *= *"[^"]*"/versionName="'$NEWNAME'"/' $MANIFEST_FILE
